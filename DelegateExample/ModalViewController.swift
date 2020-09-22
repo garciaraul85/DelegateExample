dedffb4934b2a7b6ae822dbc3b06a167f1676732
raulgarcia <garcia.raul@gmail.com>
2020-09-21 20:34:11 -0400

    2nd screen added

---
+++ DelegateExample/ModalViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ModalViewController.swift
+//  DelegateExample
+//
+//  Created by Saulo Garcia on 9/21/20.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func tappedDone(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- DelegateExample/ViewController.swift
+++ DelegateExample/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBAction func tappedShowModal(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
